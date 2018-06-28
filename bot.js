88173250815097df5e0a90e1e8173da627f5dedc
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-06-28 23:27:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '%'
+const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ATLANTIC BOT | %help`,"http://twitch.tv/S-F")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
-  console.log('+[-----------------------------------------------------------------]+')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
-  console.log('+[-----------------------------------------------------------------]+')
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
-  console.log('+[------------------------------------]+');
+  console.log('╔[════════════════════════════════════]╗');
   console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('By The Atlantic Enjoy')
   console.log('')
   console.log('Informations :')
   console.log('')
   console.log(`servers! [ " ${client.guilds.size} " ]`);
- console.log(`Users! [ " ${client.users.size} " ]`);
- console.log(`channels! [ " ${client.channels.size} " ]`);
- console.log('+[------------------------------------]+')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
   console.log('')
-  console.log('+[------------]+')
+  console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
-  console.log('+[------------]+')
+  console.log('╚[════════════]╝')
   console.log('')
   console.log('')
 });
-
 
 
 client.on('guildMemberAdd', member => {
@@ -417,18 +414,4 @@ client.on('message', message => {
     });
 
 
-
-
-
- 
- 
-
- 
-
- 
- 
- 
- 
- 
-
-client.login('process.env.BOT_TOKEN');
+client.login(process.env.BOT_TOKEN);
