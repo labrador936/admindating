274452b5aec1ddf61a39a1a96a99edc969072ab0
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-06-29 00:38:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -374,37 +374,7 @@ client.on('message', ra3d => {
 
  
  
-          const table = require('table')
-const arraySort = require('array-sort');
 
-
-         var x5bz = "%"
-         
-
-client.on('message' , async (message) => {
-
-    if(message.content.startsWith(x5bz + "invites")) {
-           if(!message.channel.guild) return
-
-  var invites = await message.guild.fetchInvites();
-
-    invites = invites.array();
-
-    arraySort(invites, 'uses', { reverse: true });
-
-    let possibleInvites = [['User Invited', 'Uses']];
-    invites.forEach(i => {
-      possibleInvites.push([i.inviter.username , i.uses]); 
-    })
-    const embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setTitle("Server Invites")
-    .addField('Top Invites:' , `\`\`\`${table.table(possibleInvites)}\`\`\``)
-    .setThumbnail(message.author.avatarURL)
-
-    message.channel.send(embed)
-    }
-});
 
  
  
