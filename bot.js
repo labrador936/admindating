b342eba4e61d30e5a15b5d782d2ba7af7885a129
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-01 06:27:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -375,18 +375,7 @@ client.on('message', ra3d => {
  
  
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  if(!message.channel.guild) return;
-  if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
-  if (message.mentions.users.size < 1) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
  
 
 
