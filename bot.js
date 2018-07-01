5b95b4062618a36b1b333172358dc3f1ac1ec259
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-01 05:32:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -374,8 +374,7 @@ client.on('message', ra3d => {
 
  
  
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
 
 client.on('message', ( message ) => {
     if( message.content == '%destroy' ){
