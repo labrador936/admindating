3ef6e2e317939543315a3669520da3b6c832670d
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-02 06:00:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
+const id = JSON.parse(fs.readFileSync("./mozo.json", "utf8"));
 const prefix = '%'
 
 
