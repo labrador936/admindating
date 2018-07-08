2e1ae14806f5497f8a5f8ec85486dd74b64fdcde
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-08 19:53:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,6 @@ const client = new Discord.Client();
 const prefix = '%'
 
 
-
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Dating Everyone Bot | %help`,"http://twitch.tv/S-F")
@@ -23,10 +21,9 @@ client.user.setGame(`Dating Everyone Bot | %help`,"http://twitch.tv/S-F")
   console.log(`Users! [ " ${client.users.size} " ]`);
   console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
+  console.log('╚[═══════════════════════]╝')
+  console.log(' Bot Is Online By Eriep')
+  console.log('╚[═══════════════════════]╝')
   console.log('')
   console.log('')
 });
