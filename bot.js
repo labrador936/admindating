8eb3369dc8f7ca791ff343a05c1f16b74f73639d
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-19 00:47:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,27 @@ client.user.setGame(`Dating Gamers Bot | %help`,"http://twitch.tv/S-F")
 });
 
 
+client.on('message', ra3d => {
+var prefix = "%";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'cc')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       }); 
+
+
+
+
+
 
   client.on('voiceStateUpdate', (codes, ReBeL) => {
 if(ReBeL.voiceChannelID !== "469190784177012755") return console.log("أيرور . ");
