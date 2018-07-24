5d05785c9d5af25263631ecf8d37dcaa10e52c52
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-24 04:26:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,6 @@ client.user.setGame(`Dating Gamers Bot | %help`,"http://twitch.tv/S-F")
 
 
 
-
 client.on('message', message => {
      if(message.content.startsWith("<dms")) {
  let args = message.content.split(" ").slice(1);
