7571404ff4a1f63d74dda23abac3c92645e0b32d
labrador936 <40704323+labrador936@users.noreply.github.com>
2018-09-28 22:18:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -656,6 +656,23 @@ client.on('message', message => {
     });
 
 
+var prefix = "-";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : sent to`); 
+ message.delete(); 
+};     
+});
+
+
+
 
 
 
