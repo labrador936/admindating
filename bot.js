9a30e2ef0bbb31858146c339deb3cc053aee7f01
Atlantic Press <40704323+labrador936@users.noreply.github.com>
2022-08-01 00:07:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,7 +125,7 @@ client.on('message', message => {
   client.on('voiceStateUpdate', (codes, ReBeL) => {
 if(ReBeL.voiceChannelID !== "1003404111393337384") return console.log("أيرور . ");
 ReBeL.guild.createChannel(ReBeL.user.username , 'voice').then((rebeeel) =>{
-    rebeeel.setParent("1003420883861373118");
+    rebeeel.setParent("403697654443540490");
 ReBeL.guild.members.get(ReBeL.id).setVoiceChannel(rebeeel.id).then((codess) =>{
   console.log("تــــــم .");
   let scan = setInterval(()=>{
